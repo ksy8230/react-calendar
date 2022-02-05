6b8c55aff49888ea2d92a4ce464f437136a8f105
ksy <ksy8230@hanmail.net>
2022-02-05 21:41:13 +0900

    cra 없이 리액트 환경 세팅하기

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <h1>creat react application without CRA 😀</h1>
+    </div>
+  );
+};
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// App 컴포넌트를 root아이디를 가진 DOM에 랜더링
+ReactDOM.render(<App />, document.getElementById("root"));
