6c4f3449eaa1cec0cf0833f053211750ef7b0d97
ksy <ksy8230@hanmail.net>
2022-02-05 23:46:45 +0900

    달력 피커 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,103 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import DatePicker from "react-datepicker";
 import "./App.css";
+import "react-datepicker/dist/react-datepicker.css";
+import { getYear, getMonth } from "date-fns";
+import { ko } from "date-fns/esm/locale";
+
+function range(start, end) {
+  let array = [];
+  for (let i = start; i < end; ++i) {
+    array.push(i);
+  }
+  return array;
+}
+
 const App = () => {
+  const ref = useRef();
+  const [startDate, setStartDate] = useState(new Date());
+  const years = range(1990, getYear(new Date()) + 1, 1);
+
+  const months = [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월",
+  ];
+
+  const handleCalendarOpen = () => {
+    console.log("time = ", startDate);
+  };
+
   return (
-    <div className="App">
-      <h1>creat react application without CRA 😀</h1>
+    <div>
+      <DatePicker
+        ref={ref}
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div
+            style={{
+              margin: 10,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              {"<"}
+            </button>
+            <select
+              value={getYear(date)}
+              onChange={({ target: { value } }) => changeYear(value)}
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <select
+              value={months[getMonth(date)]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              {">"}
+            </button>
+          </div>
+        )}
+        locale={ko}
+        dateFormat="yyyy-MM-dd aa hh:mm"
+        selected={startDate}
+        calendarClassName="custom-style" // Custom calendar class name
+        showTimeSelect
+        timeCaption="시간"
+        onChange={(date) => setStartDate(date)}
+        onCalendarOpen={handleCalendarOpen}
+      />
     </div>
   );
 };
